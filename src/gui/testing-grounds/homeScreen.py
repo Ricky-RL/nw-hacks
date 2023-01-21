dc14becb45022b7d1c8a2167fec1f47ff9d5a838
Ricky-RL <rickylin543@gmail.com>
2023-01-21 15:02:09 -0800

    added dimensions

--- src/gui/testing-grounds/homeScreen.py
+++ src/gui/testing-grounds/homeScreen.py
@@ -4,34 +4,36 @@ import customtkinter
 
 window = customtkinter.CTk()
 #window = Tk()
-window.geometry('1920x1080')
+# width = 1600 # Width 
+# height = 900 # Height
+# window.geometry(f'{width}x{height}+{-10}+{0}')
 
 # Set Background image
 bg = PhotoImage(file = "home Background.png")
 background = Label(window, image = bg)
 # background.place(x = 0, y = 0)
-# background.pack()
+background.pack()
 
-def click(event):
-    gameInput.config(state=NORMAL)
-    gameInput.delete(0,END)
+# def click(event):
+#     gameInput.config(state=NORMAL)
+#     gameInput.delete(0,END)
 
-def press(event):
-    window.destroy()
-    import movieScreen
+# def press(event):
+#     window.destroy()
+#     import movieScreen
 
 
-# Search function
-labelSearch = Label(window,text = "I like playing...")
-gameInput = Entry(window)
-gameInput.insert(0,"name of game")
-gameInput.config(state=DISABLED)
-gameInput.bind("<Button-1>", click)
+# # Search function
+# labelSearch = Label(window,text = "I like playing...")
+# gameInput = Entry(window)
+# gameInput.insert(0,"name of game")
+# gameInput.config(state=DISABLED)
+# gameInput.bind("<Button-1>", click)
 
-gameInput.bind("<Return>", press)
+# gameInput.bind("<Return>", press)
 
-labelSearch.pack()
-gameInput.pack()
+# labelSearch.pack()
+# gameInput.pack()
 
 
 window.mainloop()
