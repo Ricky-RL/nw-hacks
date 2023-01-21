a5c11551c3fd3f1202879616ea72aa0b537e6eea
willthecoder1617 <williamsunedu1617@gmail.com>
2023-01-21 15:02:46 -0800

    game parser updated

--- src/core/parse/game_parser.py
+++ src/core/parse/game_parser.py
@@ -1,2 +1,50 @@
-class GameParser:
-    a = "hi"
+import self as self
+
+from core.api.game_api import *
+
+
+class GameList:
+    def __init__(self):
+        self.games = []
+
+    def add_game(self, name, genres):
+        game = {"name": name, "genres": genres}
+        self.games.append(game)
+
+    def get_games(self):
+        return self.games
+
+    def get_game_by_name(self, name):
+        for game in self.games:
+            if game["name"] == name:
+                return game
+        return None
+
+    def get_games_by_genre(self, genre):
+        games_by_genre = []
+        for game in self.games:
+            if genre in game["genres"]:
+                games_by_genre.append(game)
+        return games_by_genre
+
+    def remove_game(self, name):
+        for i in range(len(self.games)):
+            if self.games[i]["name"] == name:
+                del self.games[i]
+                return
+        print("Game not found.")
+
+    def update_game(self, name, new_name=None, new_genres=None):
+        for i in range(len(self.games)):
+            if self.games[i]["name"] == name:
+                if new_name:
+                    self.games[i]["name"] = new_name
+                if new_genres:
+                    self.games[i]["genres"] = new_genres
+                return
+        print("Game not found.")
+
+
+movie_genres = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy",
+                "Historical", "Horror", "Musical", "Mystery", "Romance", "Science Fiction", "Thriller", "War",
+                "Western"]
