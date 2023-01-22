84a0d9e8a6015f4acdcde55e413efd0493701794
KaranveerB <52545097+KaranveerB@users.noreply.github.com>
2023-01-22 11:09:08 -0800

    Finish backend

--- src/core/parse/game_parser.py
+++ src/core/parse/game_parser.py
@@ -1,13 +1,96 @@
 from core.api.game_api import *
 from core.api.movie_api import *
 import random
-import requests
 
 
-class GameList:
+class GameParser:
+    movie_genres = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy",
+                    "Historical", "Horror", "Musical", "Mystery", "Romance", "Science Fiction", "Thriller", "War",
+                    "Western"]
+
+    movie_mapper = {
+        "Action": ["Action"],
+        "Action-Adventure": ["Action", "Adventure"],
+        "Adventure": ["Adventure", "Action"],
+        "Baseball": ["Action", "Documentary"],
+        "Basketball": ["Action", "Documentary"],
+        "Billiards": ["Drama", "Sport"],
+        "Block-Breaking": ["Western"],
+        "Bowling": ["Action", "Documentary"],
+        "Boxing": ["Action", "Drama", "Crime"],
+        "Brawler": ["Action", "Drama"],
+        "Card Game": ["Mystery", "Documentary"],
+        "Cricket": ["Documentary"],
+        "Driving/Racing": ["Action", "Thriller", "Romance"],
+        "Educational": ["Documentary", "History", "Romance"],
+        "Fighting": ["Action", "Drama", "Thriller"],
+        "First-Person Shooter": ["Action", "War", "Western", "Thriller", "History"],
+        "Fishing": ["Comedy"],
+        "Fitness": ["Romance", "Drama"],
+        "Flight Simulator": ["Romance", "Drama", "Documentary"],
+        "Football": ["Documentary"],
+        "Gambling": ["Crime", "Comedy", "Drama"],
+        "Golf": ["Family", "Drama"],
+        "Hockey": ["Family", "Action"],
+        "Light-Gun Shooter": ["Action", "Crime", "War", "Thriller", "Western"],
+        "MMORPG": ["Fantasy", "Romance", "Mystery", "Music"],
+        "MOBA": ["Family", "Action", "Adventure", "Fantasy", "War", "Thriller"],
+        "Music/Rhythm": ["Music", "Action"],
+        "Pinball": ["Documentary"],
+        "Platformer": ["Adventure", "Action"],
+        "Puzzle": ["Mystery", "Mystery", "Mystery"],
+        "Real-Time Strategy": ["War"],
+        "Role-Playing": ["Adventure", "Fantasy"],
+        "Shoot ‘Em Up": ["Action", "Crime", "War", "Thriller", "Western"],
+        "Shooter": ["Action", "Crime", "War", "Thriller", "Western"],
+        "Simulation": ["Romance", "Science Fiction"],
+        "Skateboarding": ["Documentary", "Western", "Action"],
+        "Snowboarding/Skiing": ["Adventure", "Thriller"],
+        "Soccer": ["Drama", "Comedy"],
+        "Sports": ["Documentary", "Drama", "Family"],
+        "Strategy": ["Documentary", "Science Fiction", "War", "Family"],
+        "Tennis": ["Thriller", "Family", "Adventure"],
+        "Text Adventure": ["Horror", "Fantasy", "Family", "Science Fiction", "Thriller"],
+        "Track & Field": ["Action", "Family", "Drama"],
+        "Trivia/Board Game": ["Mystery", "Family"],
+        "Vehicular Combat": ["Action", "War", "Crime", "Drama"],
+        "Wrestling": ["Action", "War", "Drama"],
+        "Abstract": ["Fiction"],
+        "Adult": ["Romance", "Drama", "Family"],
+        "Alternative Historical": ["War", "Documentary"],
+        "Anime": ["Animation", "Animation", "Animation", "Animation"],
+        "Aquatic": ["Music"],
+        "Civil War": ["War"],
+        "Comedy": ["Comedy"],
+        "Comic Book": ["Action", "Adventure"],
+        "Crime": ["Crime", "Thriller"],
+        "Cyberpunk": ["Science Fiction", "Science Fiction", "Science Fiction"],
+        "Dating": ["Romance", "Comedy", "Family", "Romance", "Romance", "Romance"],
+        "Egyptian": ["History", "Mystery", "Documentary"],
+        "Espionage": ["Mystery", "Thriller"],
+        "Fantasy": ["Fantasy", "Fantasy"],
+        "Game Show": ["Family", "Comedy"],
+        "Horror": ["Horror", "Horror"],
+        "Management": ["Western"],
+        "Martial Arts": ["Action", "Thriller"],
+        "Mayan": ["History", "Mystery", "Documentary"],
+        "Medieval": ["War", "History", "Thriller", "Documentary"],
+        "Modern Military": ["War", "History", "Action", "Thriller"],
+        "Motorsports": ["Action", "Drama", "Action", "Western"],
+        "Post-Apocalyptic": ["Action", "Horror", "Thriller"],
+        "Prehistoric": ["History", "Documentary"],
+        "Sci-Fi": ["Science Fiction", "Science Fiction"],
+        "Steampunk": ["Science Fiction, Adventure"],
+        "Superhero": ["Action", "Mystery", "Drama"],
+        "Vietnam": ["History", "Documentary"],
+        "Western": ["Western"],
+        "World War II": ["War", "Action", "Thriller", "Horror"]
+    }
+
     def __init__(self):
         self.gameAPI = GameAPI()
         self.games = []
+        self.movieAPI = MovieAPI()
 
     def add_game(self, name):
         self.games.append(self.gameAPI.get_game_by_name(name))
@@ -32,120 +115,24 @@ class GameList:
                 return
         print("Game not found.")
 
+    def get_movie_recommendations(self):
+        movie_genres = []
+        for game in self.games:
+            for genre in game.genres:
+                g_list = self.movie_mapper.get(genre)
+                for g in g_list:
+                    movie_genres.append(g)
+        unique_genres = list(set(movie_genres))
+        if len(unique_genres) < 4:
+            return self.movieAPI.get_movie_recommendations(unique_genres)
+        else:
+            return self.movieAPI.get_movie_recommendations(random.sample(unique_genres, 3))
 
-movie_genres = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy",
-                "Historical", "Horror", "Musical", "Mystery", "Romance", "Science Fiction", "Thriller", "War",
-                "Western"]
-
-# gameAPI = GameAPI()
-# print(gameAPI.get_game_by_name("The snowboard Game"))
-movie_mapper = {
-    "Action": ["Action"],
-    "Action-Adventure": ["Action", "Adventure"],
-    "Adventure": ["Adventure", "Action"],
-    "Baseball": ["Action", "Documentary"],
-    "Basketball": ["Action", "Documentary"],
-    "Billiards": ["Drama", "Sport"],
-    "Block-Breaking": ["Western"],
-    "Bowling": ["Action", "Documentary"],
-    "Boxing": ["Action", "Drama", "Crime"],
-    "Brawler": ["Action", "Drama"],
-    "Card Game": ["Mystery", "Documentary"],
-    "Cricket": ["Documentary"],
-    "Driving/Racing": ["Action", "Thriller", "Romance"],
-    "Educational": ["Documentary", "History", "Romance"],
-    "Fighting": ["Action", "Drama", "Thriller"],
-    "First-Person Shooter": ["Action", "War", "Western", "Thriller", "History"],
-    "Fishing": ["Comedy"],
-    "Fitness": ["Romance", "Drama"],
-    "Flight Simulator": ["Romance", "Drama", "Documentary"],
-    "Football": ["Documentary"],
-    "Gambling": ["Crime", "Comedy", "Drama"],
-    "Golf": ["Family", "Drama"],
-    "Hockey": ["Family", "Action"],
-    "Light-Gun Shooter": ["Action", "Crime", "War", "Thriller", "Western"],
-    "MMORPG": ["Fantasy", "Romance", "Mystery", "Music"],
-    "MOBA": ["Family", "Action", "Adventure", "Fantasy", "War", "Thriller"],
-    "Music/Rhythm": ["Music", "Action"],
-    "Pinball": ["Documentary"],
-    "Platformer": ["Adventure", "Action"],
-    "Puzzle": ["Mystery", "Mystery", "Mystery"],
-    "Real-Time Strategy": ["War"],
-    "Role-Playing": ["Adventure", "Fantasy"],
-    "Shoot ‘Em Up": ["Action", "Crime", "War", "Thriller", "Western"],
-    "Shooter": ["Action", "Crime", "War", "Thriller", "Western"],
-    "Simulation": ["Romance", "Science Fiction"],
-    "Skateboarding": ["Documentary", "Western", "Action"],
-    "Snowboarding/Skiing": ["Adventure", "Thriller"],
-    "Soccer": ["Drama", "Comedy"],
-    "Sports": ["Documentary", "Drama", "Family"],
-    "Strategy": ["Documentary", "Science Fiction", "War", "Family"],
-    "Tennis": ["Thriller", "Family", "Adventure"],
-    "Text Adventure": ["Horror", "Fantasy", "Family", "Science Fiction", "Thriller"],
-    "Track & Field": ["Action", "Family", "Drama"],
-    "Trivia/Board Game": ["Mystery", "Family"],
-    "Vehicular Combat": ["Action", "War", "Crime", "Drama"],
-    "Wrestling": ["Action", "War", "Drama"],
-    "Abstract": ["Fiction"],
-    "Adult": ["Romance", "Drama", "Family"],
-    "Alternative Historical": ["War", "Documentary"],
-    "Anime": ["Animation", "Animation", "Animation", "Animation"],
-    "Aquatic": ["Music"],
-    "Civil War": ["War"],
-    "Comedy": ["Comedy"],
-    "Comic Book": ["Action", "Adventure"],
-    "Crime": ["Crime", "Thriller"],
-    "Cyberpunk": ["Science Fiction", "Science Fiction", "Science Fiction"],
-    "Dating": ["Romance", "Comedy", "Family", "Romance", "Romance", "Romance"],
-    "Egyptian": ["History", "Mystery", "Documentary"],
-    "Espionage": ["Mystery", "Thriller"],
-    "Fantasy": ["Fantasy", "Fantasy"],
-    "Game Show": ["Family", "Comedy"],
-    "Horror": ["Horror", "Horror"],
-    "Management": ["Western"],
-    "Martial Arts": ["Action", "Thriller"],
-    "Mayan": ["History", "Mystery", "Documentary"],
-    "Medieval": ["War", "History", "Thriller", "Documentary"],
-    "Modern Military": ["War", "History", "Action", "Thriller"],
-    "Motorsports": ["Action", "Drama", "Action", "Western"],
-    "Post-Apocalyptic": ["Action", "Horror", "Thriller"],
-    "Prehistoric": ["History", "Documentary"],
-    "Sci-Fi": ["Science Fiction", "Science Fiction"],
-    "Steampunk": ["Science Fiction, Adventure"],
-    "Superhero": ["Action", "Mystery", "Drama"],
-    "Vietnam": ["History", "Documentary"],
-    "Western": ["Western"],
-    "World War II": ["War", "Action", "Thriller", "Horror"]
-}
-
-gameAPI = GameAPI()
-movieAPI = MovieAPI()
-
-game = input("Find movies like this type of video game:")
-
-while not gameAPI.get_game_by_name(game):
-    game = input("Invalid game, try again:")
-
-# try:
-#     gameAPI.get_game_by_name(game).genres
-# except:
-#     quit()
-
-movies = []
-for genre in gameAPI.get_game_by_name(game).genres:
-    movies.append(movie_mapper.get(genre))
-
-flatmovies = []
-
-for sublist in movies:
-    for item in sublist:
-        flatmovies.append(item)
-
-movies.clear()
+g = GameParser()
 for i in range(3):
-    movies.append(random.choice(flatmovies))
+    game = input("Find movies like this type of video game:")
+    g.add_game(game)
 
-print(flatmovies)
-print(movies)
-
-print(movieAPI.get_movie_recommendations(movies))
+x = g.get_movie_recommendations()
+for a in x:
+    print(a.name)
